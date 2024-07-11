e29b1f0f822ef313b0a91b1d78c58df53f24ce71
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-07-11 10:54:19 -0500

    feat: implement custom app bar with react native paper

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {DetailsScreen, HomeScreen} from './src/screens';
 import {Provider} from 'react-native-paper';
+import CustomNavigationBar from './src/custom-appbar';
 
 const Stack = createStackNavigator();
 
@@ -10,7 +11,12 @@ export default function App() {
   return (
     <Provider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            // eslint-disable-next-line react/no-unstable-nested-components
+            header: props => <CustomNavigationBar {...props} />,
+          }}>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Details" component={DetailsScreen} />
         </Stack.Navigator>
